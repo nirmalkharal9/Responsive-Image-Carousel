a2cb2fd428c0e4eb95aafec48219607b80fe2c10
nirmalkharal9 <nirmalkharal2058@gmil..com>
2024-08-05 12:04:48 +0545

    carousal all files added

--- script.js
+++ script.js
@@ -22,3 +22,35 @@ function nextSlide() {
 function prevSlide() {
   showSlide(currentSlide - 1);
 }
+
+
+
+function startAutoSlide() {
+  autoSlideInterval = setInterval(nextSlide, 3000); // Change slide every 3 seconds
+}
+
+function stopAutoSlide() {
+  clearInterval(autoSlideInterval);
+}
+
+// Start the auto-slide when the page loads
+window.onload = function () {
+  startAutoSlide();
+};
+
+// Stop auto-slide when user interacts with the controls
+document
+  .querySelector(".carousel-control.prev")
+  .addEventListener("click", function () {
+    stopAutoSlide();
+    prevSlide();
+    startAutoSlide();
+  });
+
+document
+  .querySelector(".carousel-control.next")
+  .addEventListener("click", function () {
+    stopAutoSlide();
+    nextSlide();
+    startAutoSlide();
+  });
